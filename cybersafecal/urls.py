c433dd38983a24c985cc4880ebf2bbfd33df3c3e
madvirus-ops <edwinayabie1@gmail.com>
2022-08-24 19:13:46 +0100

    created email model

--- cybersafecal/urls.py
+++ cybersafecal/urls.py
@@ -19,6 +19,6 @@ from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
-    path('home',views.home, name='home2')
+    path('', views.home, name='home'),
+    path('home',views.home, name='home2'),
 ]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+class Usemail(models.Model):
+
+    emaillist = models.EmailField(max_length=254)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        # verbose_name = _("user email")
+        # verbose_name_plural = _("s")
+        # ordering = ('-email')
+        pass
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
